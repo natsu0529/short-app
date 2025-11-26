623a70b0e0b254b549cf91dfbdc488b3fdac2e38
鈴木夏大 <t233025@edu.kaiyodai.ac.jp>
2025-11-26 15:17:09 +0900

    Initialize accounts app with models, views, admin, and migrations

---
+++ accounts/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+# accounts/models.py
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class CustomUser(AbstractUser):
+    user_id = models.AutoField(primary_key=True)
+    user_name = models.CharField(max_length=50)
+    user_rank = models.CharField(max_length=20, blank=True)
+    user_level = models.PositiveIntegerField(default=1)
+    user_mail = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)  # AbstractUser ですでにあるので省略可
+    good = models.PositiveIntegerField(default=0)
+    user_URL = models.URLField(blank=True)
+    user_bio = models.TextField(blank=True)
