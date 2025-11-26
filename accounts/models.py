36ef42f36c372587fe9fd5fc215fcf66425f0423
鈴木夏大 <t233025@edu.kaiyodai.ac.jp>
2025-11-26 16:24:21 +0900

    Remove redundant import of models in accounts/models.py

--- accounts/models.py
+++ accounts/models.py
@@ -3,7 +3,6 @@ from django.db import models
 # Create your models here.
 # accounts/models.py
 from django.contrib.auth.models import AbstractUser
-from django.db import models
 
 
 class CustomUser(AbstractUser):
